98b0276805dc9344e03f23492e82859cd95fd56a
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-04 15:57:03 +0530

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,12 +1,13 @@
-#sudo su
 /usr/bin/apt-get update -y
 mkdir /opt/jenkins && cd /opt/jenkins
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 /usr/bin/apt-get update -y
-/usr/bin/apt-get install default-jdk -y
+/usr/bin/add-apt-repository ppa:openjdk-r/ppa
+/usr/bin/apt-get update -y
+/usr/bin/apt-get install openjdk-8-jdk -y
 /usr/bin/apt-get install jenkins -y
 service jenkins start
-sed -i '$d' /etc/default/jenkins 
-echo 'JENKINS_ARGS="--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT --prefix=/jenkins"' >> /etc/default/jenkins 
+sed -i '$d' /etc/default/jenkins
+echo 'JENKINS_ARGS="--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT --prefix=/jenkins"' >> /etc/default/jenkins
 service jenkins restart
