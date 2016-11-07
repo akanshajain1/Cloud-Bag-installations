b0b2c9b44b9c7621a0b8ea83519a8cb479cabcfb
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-07 12:19:31 +0530

    Update install-sonar-without-mysql.sh

--- install-sonar-without-mysql.sh
+++ install-sonar-without-mysql.sh
@@ -7,4 +7,6 @@ wget https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.0.zip
 /usr/bin/apt-get install unzip
 unzip sonarqube-6.0.zip
 ln -s sonarqube-6.0 sonar
+echo " " >> /opt/sonar/sonar/conf/sonar.properties
+echo "sonar.web.context=/sonar" >> /opt/sonar/sonar/conf/sonar.properties
 /opt/sonar/sonar/bin/linux-x86-64/sonar.sh start
