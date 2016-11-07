6045b81b9ed8be4de18765325c0dd938e9cc57ac
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-07 12:05:15 +0530

    Create install-sonar-without-mysql.sh

---
+++ install-sonar-without-mysql.sh
@@ -0,0 +1,10 @@
+/usr/bin/apt-get update -y
+/usr/bin/add-apt-repository ppa:openjdk-r/ppa
+/usr/bin/apt-get update -y
+/usr/bin/apt-get install openjdk-8-jdk -y
+mkdir /opt/sonar && cd /opt/sonar
+wget https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.0.zip
+/usr/bin/apt-get install unzip
+unzip sonarqube-6.0.zip
+ln -s sonarqube-6.0 sonar
+/opt/sonar/sonar/bin/linux-x86-64/sonar.sh start
