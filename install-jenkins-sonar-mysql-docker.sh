bab2d309bf12902b9348f89c1799a831170e0535
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-11 17:44:04 +0530

    Create install-jenkins-sonar-mysql-docker.sh

---
+++ install-jenkins-sonar-mysql-docker.sh
@@ -0,0 +1,6 @@
+# sudo su
+/usr/bin/apt-get update -y
+wget -qO- https://get.docker.com/ | sh
+docker run --name jenkins  -p 8080:8080  jenkins --prefix=/jenkins
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=welcome123 -d mysql
+docker run --name sonar -d -p 9000:9000 sonarqube --link mysql:db sonarqube
