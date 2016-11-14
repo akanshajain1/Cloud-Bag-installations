4317de094d8a915d83be7651359e8a67afe7b7c8
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-14 13:24:43 +0000

    Update install-jenkins-docker.sh

--- install-jenkins-docker.sh
+++ install-jenkins-docker.sh
@@ -1,4 +1,4 @@
 # sudo su
 /usr/bin/apt-get update -y
 wget -qO- https://get.docker.com/ | sh
-docker run --name jenkins  -p 8080:8080  jenkins --prefix=/jenkins
+sudo docker run --name jenkins  -p 8080:8080  jenkins --prefix=/jenkins
