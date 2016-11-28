2181fd300e1cb58e7cfc4ce3911b45496d058359
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-28 11:30:49 +0530

    Update install-nginx-jenkins-sonar-mysql-docker.sh

--- install-nginx-jenkins-sonar-mysql-docker.sh
+++ install-nginx-jenkins-sonar-mysql-docker.sh
@@ -12,7 +12,7 @@ sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my
 sudo service mysql restart
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
 echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
-docker run --name sonar -d -p 9000:9000 -e DB_USER=sonar -e DB_PWD=sonar -e DB_URL=10.0.3.6 -e DB_PORT=3306  preetick/sonar-devops:v18
+docker run --name sonar -d -p 9000:9000 -e DB_USER=sonar -e DB_PWD=sonar -e DB_URL=127.0.0.1 -e DB_PORT=3306  preetick/sonar-devops:v18
 apt-get update
 IP_ADDR=sh curl ipecho.net/plain
 docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=IP_ADDR -e JENKINS_ADDR=IP_ADDR -e SONAR_ADDR=IP_ADDR preetick/nginx-devops:v13
