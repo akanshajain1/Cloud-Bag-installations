c8add507b33fe55f8c455dbc559a30a70a125e21
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-11-30 19:38:24 +0530

    Update install-nginx-config.sh

--- install-nginx-config.sh
+++ install-nginx-config.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt-get install nginx
-sudo cd /etc/nginx/conf.d
-echo "server { listen 80; client_max_body_size 20M; server_name  devopsformyorg.southeastasia.cloudapp.azure.com;" >> /etc/nginx/conf.d/devtools.conf
-echo "location /jenkins { proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; proxy_set_header Host $http_host;" >> /etc/nginx/conf.d/devtools.conf
-echo "proxy_redirect off; proxy_pass http://10.0.2.8:8080; }" >> /etc/nginx/conf.d/devtools.conf
-echo "location /sonar {proxy_pass http://10.0.2.7:9000;}" >> /etc/nginx/conf.d/devtools.conf
+sudo echo "server { listen 80; client_max_body_size 20M; server_name  devopsformyorg.southeastasia.cloudapp.azure.com;" >> /etc/nginx/conf.d/devtools.conf
+sudo echo "location /jenkins { proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; proxy_set_header Host $http_host;" >> /etc/nginx/conf.d/devtools.conf
+sudo echo "proxy_redirect off; proxy_pass http://10.0.2.8:8080; }" >> /etc/nginx/conf.d/devtools.conf
+sudo echo "location /sonar {proxy_pass http://10.0.2.7:9000;}" >> /etc/nginx/conf.d/devtools.conf
 sudo service nginx restart
