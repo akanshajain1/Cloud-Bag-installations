e5ddc462fa7f43be4f840aca3326c5d46096c7ab
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-01 17:15:29 +0530

    Update install-nginx-config.sh

--- install-nginx-config.sh
+++ install-nginx-config.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install nginx
+sudo touch /etc/nginx/conf.d/devtools.conf
 sudo echo "server { listen 80; client_max_body_size 20M; server_name  devopsformyorg.southeastasia.cloudapp.azure.com;" >> /etc/nginx/conf.d/devtools.conf
 sudo echo "location /jenkins { proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; proxy_set_header Host $http_host;" >> /etc/nginx/conf.d/devtools.conf
 sudo echo "proxy_redirect off; proxy_pass http://10.0.2.8:8080; }" >> /etc/nginx/conf.d/devtools.conf
