16319fff1e0d858475148c7f5164422bcde9d44f
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-09 12:54:36 +0530

    Update install-tomcat8.sh

--- install-tomcat8.sh
+++ install-tomcat8.sh
@@ -1,6 +1,6 @@
-wget http://mirrors.ibiblio.org/apache/tomcat/tomcat-8/v8.5.6/bin/apache-tomcat-8.5.6.tar.gz
-tar xvzf apache-tomcat-8.5.6.tar.gz
-mv apache-tomcat-8.5.6 /opt/tomcat
+wget http://mirrors.ibiblio.org/apache/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz
+tar xvzf apache-tomcat-8.5.9.tar.gz
+mv apache-tomcat-8.5.9 /opt/tomcat
 mkdir /opt/java
 cd /opt/java
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
