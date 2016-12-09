8ce93210dd42fc1d28f13ffc04b6d24908e388c7
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-09 13:07:05 +0530

    Update install-nginx-config.sh

--- install-nginx-config.sh
+++ install-nginx-config.sh
@@ -4,5 +4,5 @@ sudo apt-get install nginx -y
 sudo wget https://raw.githubusercontent.com/DevOps-Mindtree/Cloud-Bag-installations/master/nginxconfigscript.sh?_sm_au_=iRVkD8DPLjJBMnRq
 sudo mv nginxconfigscript.sh?_sm_au_=iRVkD8DPLjJBMnRq nginxconfigscript.sh
 sudo chmod 755 nginxconfigscript.sh
-sudo sh nginxconfigscript.sh
+sudo sh nginxconfigscript.sh $1
 sudo service nginx restart
