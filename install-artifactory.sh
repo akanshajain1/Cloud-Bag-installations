1112ae63528cbcfec3e3bf060cb048b6b6c0adfe
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-12 11:07:20 +0530

    Update install-artifactory.sh

--- install-artifactory.sh
+++ install-artifactory.sh
@@ -10,6 +10,6 @@ unzip jfrog-artifactory-oss-4.14.0.zip
 cd /opt/artifactory-oss-4.14.0/bin 
 ./installService.sh 
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/" >> /etc/opt/jfrog/artifactory/default 
-sed -i -e 's/8081/8080/' /opt/artifactory-oss-4.14.0/tomcat/conf/server.xml 
+#sed -i -e 's/8081/8080/' /opt/artifactory-oss-4.14.0/tomcat/conf/server.xml 
 service artifactory check 
 service artifactory start
