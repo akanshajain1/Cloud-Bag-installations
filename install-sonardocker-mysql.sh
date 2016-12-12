ff33c04cb73bee838aa0139ce2513b4362cb537b
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-12 15:22:37 +0530

    Update install-sonardocker-mysql.sh

--- install-sonardocker-mysql.sh
+++ install-sonardocker-mysql.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 apt-get update
-
 # set up a silent install of MySQL
-
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password welcome123 | debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password welcome123 | debconf-set-selections
-
 # install the LAMP stack
 apt-get -y install mysql-server-5.6 
-
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-
 sudo service mysql restart
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
-
 echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
-
 /usr/bin/apt-get update -y
 wget -qO- https://get.docker.com/ | sh
 docker run -d --name sonarqube \
-    -p 9000:9000 -p 9092:9092 \
-    -e SONARQUBE_JDBC_USERNAME=sonar \
-    -e SONARQUBE_JDBC_PASSWORD=sonar \
-    -e SONARQUBE_JDBC_URL=jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance \
-    sonarqube
+    -p 9000:9000 \
+    -e SONARQUBE_JDBC_USERNAME='sonar' \
+    -e SONARQUBE_JDBC_PASSWORD='sonar' \
+    -e SONARQUBE_JDBC_URL='jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance' \
+    sonarqube:latest
 
