ca1f59a81228a1a3a8c09c2c667394970fee4a35
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-12 12:28:06 +0530

    Update install-nginx-docker.sh

--- install-nginx-docker.sh
+++ install-nginx-docker.sh
@@ -2,5 +2,4 @@
 apt-get update
 wget -qO- https://get.docker.com/ | sh
 #IP_ADDR=sh curl ipecho.net/plain
-sudo docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=devopsformyorg.southeastasia.cloudapp.azure.com -e JENKINS_ADDR=devopsformyorg.southeastasia.cloudapp.azure.com -e SONAR_ADDR=devopsformyorg.southeastasia.cloudapp.azure.com preetick/nginx-devops:v13
-
+sudo docker run --name nginx_devops -d -p 80:80 -e NGINX_ADDR=$1.southeastasia.cloudapp.azure.com -e JENKINS_ADDR=$1.southeastasia.cloudapp.azure.com -e SONAR_ADDR=$1.southeastasia.cloudapp.azure.com preetick/nginx-devops:v13
