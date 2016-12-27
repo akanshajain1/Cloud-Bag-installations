8aa9ef50a11b647024904cd77a75dc89ffb9c18c
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2016-12-27 09:48:26 +0000

    Update install-sonarwithmysql-docker-image.sh

--- install-sonarwithmysql-docker-image.sh
+++ install-sonarwithmysql-docker-image.sh
@@ -1,3 +1,4 @@
+# MAINTAINER : Harish Kumar Garg (M1030936)
 sudo /usr/bin/apt-get update -y
 wget -qO- https://get.docker.com/ | sh
 sudo docker run -d -p 3306:3306 -p 9000:9000 --name sonar-mysql devopsmt/sonar6.0-mysql5.6
