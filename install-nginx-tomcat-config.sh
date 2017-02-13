9c04109dee6a51de6e39474d944fbd2117eacab7
DevOps-Mindtree <Cloud.Bag@mindtree.com>
2017-02-13 15:10:17 +0530

    Update install-nginx-tomcat-config.sh

--- install-nginx-tomcat-config.sh
+++ install-nginx-tomcat-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install nginx -y
-sudo wget https://raw.githubusercontent.com/DevOps-Mindtree/Cloud-Bag-installations/master/nginxconfigscript.sh?_sm_au_=iRVkD8DPLjJBMnRq
-sudo mv nginxconfigscript.sh?_sm_au_=iRVkD8DPLjJBMnRq nginxconfigscript.sh
+sudo wget https://raw.githubusercontent.com/DevOps-Mindtree/Cloud-Bag-installations/master/nginxtomcatconfig.sh?_sm_au_=ifVk6rZDjPQ0SSP5
+sudo mv nginxtomcatconfig.sh?_sm_au_=ifVk6rZDjPQ0SSP5 nginxconfigscript.sh
 sudo chmod 755 nginxconfigscript.sh
 sudo sh nginxconfigscript.sh $1
 sudo service nginx restart
